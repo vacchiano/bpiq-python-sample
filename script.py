5c9edccfe065d55467f8971fe27b0f769953a2c3
Vacchiano <djvacchiano@gmail.com>
2025-07-08 17:55:18 -0500

    init commit

---
+++ script.py
@@ -0,0 +1,22 @@
+import json
+
+# storing BPIQ API key in credentials.py file
+import credentials
+
+import requests
+
+url = 'https://api.bpiq.com/api/v1/drugs/'
+headers = {
+    'Authorization': f'Token {credentials.API_TOKEN}'
+}
+
+response = requests.get(url, headers=headers)
+
+# Optional: handle response
+if response.status_code == 200:
+    data = response.json()
+    print(json.dumps(data, indent=2))
+    with open("bpiq_response.json", "w+") as f:
+        json.dump(data, f, indent=2)
+else:
+    print(f"Request failed with status code {response.status_code}")
